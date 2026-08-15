53307e029121ffe26fe8b4d9706be6c0fab8827f
jayzone91 <register@j4yz0n3.de>
2026-08-15 14:07:17 +0200

    imports hinzugefügt

--- home/jay.nix
+++ home/jay.nix
@@ -1,6 +1,13 @@
 {config, pkgs, inputs, ...}:
 
 {
+  imports = [
+  ./modules/shell.nix
+  ./modules/starship.nix
+  ./modules/cli.nix
+  ./modules/neovim.nix
+  ];
+
   home = {
     stateVersion = "26.05";
 

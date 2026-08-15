a151663b982b8b3a2dd386dca6a438db21f0d204
jayzone91 <register@j4yz0n3.de>
2026-08-15 11:25:24 +0200

    Init Nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "MacOS Config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nix-darwin, ... }:
+  {};
+}

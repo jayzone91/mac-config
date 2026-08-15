d486fa41e9f3cd2ccdbae2d8f4ea25ef1828a2d4
jayzone91 <register@j4yz0n3.de>
2026-08-15 11:43:22 +0200

    macbook host module

--- hosts/MacbookPro/default.nix
+++ hosts/MacbookPro/default.nix
@@ -2,4 +2,12 @@
 
 {
   system.stateVersion = 6;
+  system.primaryUser = "jay";
+
+  users.users.jay = {
+    home = "/Users/jay";
+  };
+
+  networking.hostName = "MacbookPro";
+  networking.localHostName = "MacbookPro";
 }
